69d61f9d12666fbc4a07d45307abe58ffc89ff37
Marius Mateoc <mateoc.marius@gmail.com>
2020-04-25 00:45:19 +0200

    macos: Separate misc CLI and misc GUI
    - Add more CLI apps from old dotfiles like: unrar, rename, rsync, wget

--- src/os/install/macos/misc.sh
+++ src/os/install/macos/misc.sh
@@ -6,13 +6,19 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-print_in_purple "\n   Miscellaneous\n\n"
+print_in_purple "\n   Miscellaneous GUI Tools\n\n"
 
 brew_install "Alfred" "alfred" "homebrew/cask" "cask"
 brew_install "Android File Transfer" "android-file-transfer" "homebrew/cask" "cask"
 brew_install "AppCleaner" "appcleaner" "homebrew/cask" "cask"
+brew_install "Dash" "dash" "homebrew/cask" "cask"
 brew_install "Dropbox" "dropbox" "homebrew/cask" "cask"
+brew_install "iTerm" "iterm2" "homebrew/cask" "cask"
 brew_install "Recordit" "recordit" "homebrew/cask" "cask"
+brew_install "Sequel Pro" "sequel-pro" "homebrew/cask" "cask"
 brew_install "Skype" "skype" "homebrew/cask" "cask"
 brew_install "Transmission" "transmission" "homebrew/cask" "cask"
+brew_install "Virtualbox" "virtualbox" "homebrew/cask" "cask"
+brew_install "Visual Studio Code" "visual-studio-code" "homebrew/cask" "cask"
+brew_install "Visual Studio Code Insiders" "visual-studio-code-insiders" "homebrew/cask" "cask"
 brew_install "VLC" "vlc" "homebrew/cask" "cask"
