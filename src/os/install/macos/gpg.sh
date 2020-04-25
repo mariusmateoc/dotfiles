877d4d25f4e217c4a67b4d9de63f591ed6e855cc
Marius Mateoc <mateoc.marius@gmail.com>
2020-04-26 01:52:01 +0200

    chore: Add URL on how to setup gpg and pinentry-mac

--- src/os/install/macos/gpg.sh
+++ src/os/install/macos/gpg.sh
@@ -8,5 +8,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   GPG\n\n"
 
+# https://dev.to/wes/how2-using-gpg-on-macos-without-gpgtools-428f
+
 brew_install "GPG" "gpg"
 brew_install "Pinentry" "pinentry-mac"
