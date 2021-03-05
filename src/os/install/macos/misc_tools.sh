4d76d600ae75e435513a593070cdb8cd2ada1c70
Marius Mateoc <mateoc.marius@gmail.com>
2021-03-05 18:22:28 +0100

    feat: Add htop to monitor computer

--- src/os/install/macos/misc_tools.sh
+++ src/os/install/macos/misc_tools.sh
@@ -9,9 +9,10 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 print_in_purple "\n   Miscellaneous CLI Tools\n\n"
 
 brew_install "Grep" "grep"
-brew_install "ShellCheck" "shellcheck"
+brew_install "HTOP" "htop"
 brew_install "Rename" "rename"
 brew_install "Rsync" "rsync"
+brew_install "ShellCheck" "shellcheck"
 brew_install "Tree" "tree"
 brew_install "Unrar" "unrar"
 brew_install "Vagrant" "vagrant" "--cask"
