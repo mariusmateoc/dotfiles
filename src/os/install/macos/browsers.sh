9469e9ef95b2dbc53f45ed9cfdc7174bf3d11d60
Marius Mateoc <mateoc.marius@gmail.com>
2023-02-17 23:22:43 +0100

    fix cask versions for canary browsers

--- src/os/install/macos/browsers.sh
+++ src/os/install/macos/browsers.sh
@@ -13,14 +13,14 @@ print_in_purple "\n   Browsers\n"
 printf "\n"
 
 brew_install "Chrome" "google-chrome" "--cask"
-brew_install "Chrome Canary" "google-chrome-canary" "--cask"
+brew_install "Chrome Canary" "google-chrome-canary" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 printf "\n"
 
 brew_install "Firefox" "firefox" "--cask"
-brew_install "Firefox Developer" "firefox-developer-edition" "--cask"
+brew_install "Firefox Developer" "firefox-developer-edition" "--cask" "homebrew/cask-versions"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -29,5 +29,5 @@ brew_install "Firefox Developer" "firefox-developer-edition" "--cask"
 
 if is_supported_version "$(get_os_version)" "10.11.4"; then
     printf "\n"
-    brew_install "Safari Technology Preview" "safari-technology-preview" "--cask"
+    brew_install "Safari Technology Preview" "safari-technology-preview" "--cask" "homebrew/cask-versions"
 fi
