8a18f984d4136265e7988a3f8afcf12053ca7377
Marius Mateoc <mateoc.marius@gmail.com>
2023-06-18 20:05:59 +0200

    macos: [fonts] fix fonts install

--- src/os/install/macos/fonts.sh
+++ src/os/install/macos/fonts.sh
@@ -8,37 +8,37 @@ cd "$(dirname "${BASH_SOURCE[0]}")" \
 
 print_in_purple "\n   Fonts\n\n"
 
-brew_install "Bitstream Vera" "font-bitstream-vera" "--cask"
-brew_install "Droid Sans Mono Powerline" "font-droid-sans-mono-for-powerline" "--cask"
+brew_install "Bitstream Vera" "font-bitstream-vera" "--" "homebrew/cask-fonts"
+brew_install "Droid Sans Mono Powerline" "font-droid-sans-mono-for-powerline" "--" "homebrew/cask-fonts"
 
-brew_install "Fira Code" "font-fira-code" "--cask"
-brew_install "Fira Mono" "font-fira-mono" "--cask"
-brew_install "Fira Sans" "font-fira-sans" "--cask"
+brew_install "Fira Code" "font-fira-code" "--" "homebrew/cask-fonts"
+brew_install "Fira Mono" "font-fira-mono" "--" "homebrew/cask-fonts"
+brew_install "Fira Sans" "font-fira-sans" "--" "homebrew/cask-fonts"
 
-brew_install "FontAwesome" "font-fontawesome" "--cask"
+brew_install "FontAwesome" "font-fontawesome" "--" "homebrew/cask-fonts"
 
-brew_install "Lato" "font-lato" "--cask"
+brew_install "Lato" "font-lato" "--" "homebrew/cask-fonts"
 
-brew_install "Montserrat" "font-montserrat" "--cask"
+brew_install "Montserrat" "font-montserrat" "--" "homebrew/cask-fonts"
 
-brew_install "Noto Serif" "font-noto-serif" "--cask"
-brew_install "Noto Sans" "font-noto-sans" "--cask"
-brew_install "Noto Mono" "font-noto-mono" "--cask"
+brew_install "Noto Serif" "font-noto-serif" "--" "homebrew/cask-fonts"
+brew_install "Noto Sans" "font-noto-sans" "--" "homebrew/cask-fonts"
+brew_install "Noto Mono" "font-noto-mono" "--" "homebrew/cask-fonts"
 
-brew_install "Open Sans" "font-open-sans" "--cask"
+brew_install "Open Sans" "font-open-sans" "--" "homebrew/cask-fonts"
 
-brew_install "PT Serif" "font-pt-serif" "--cask"
-brew_install "PT Sans" "font-pt-sans" "--cask"
-brew_install "PT Mono" "font-pt-mono" "--cask"
+brew_install "PT Serif" "font-pt-serif" "--" "homebrew/cask-fonts"
+brew_install "PT Sans" "font-pt-sans" "--" "homebrew/cask-fonts"
+brew_install "PT Mono" "font-pt-mono" "--" "homebrew/cask-fonts"
 
-brew_install "Raleway" "font-raleway" "--cask"
+brew_install "Raleway" "font-raleway" "--" "homebrew/cask-fonts"
 
-brew_install "Roboto" "font-roboto" "--cask"
-brew_install "Roboto Mono" "font-roboto-mono" "--cask"
-brew_install "Roboto Slab" "font-roboto-slab" "--cask"
+brew_install "Roboto" "font-roboto" "--" "homebrew/cask-fonts"
+brew_install "Roboto Mono" "font-roboto-mono" "--" "homebrew/cask-fonts"
+brew_install "Roboto Slab" "font-roboto-slab" "--" "homebrew/cask-fonts"
 
-brew_install "Source Code Pro" "font-source-code-pro" "--cask"
-brew_install "Source Serif Pro" "font-source-serif-pro" "--cask"
-brew_install "Source Sans Pro" "font-source-sans-pro" "--cask"
+brew_install "Source Code Pro" "font-source-code-pro" "--" "homebrew/cask-fonts"
+brew_install "Source Serif Pro" "font-source-serif-pro" "--" "homebrew/cask-fonts"
+brew_install "Source Sans Pro" "font-source-sans-pro" "--" "homebrew/cask-fonts"
 
-brew_install "Ubuntu" "font-ubuntu" "--cask"
+brew_install "Ubuntu" "font-ubuntu" "--" "homebrew/cask-fonts"
